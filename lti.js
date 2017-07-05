c3c51b7e3fe68ebe5275468269d803b66ab0fc1d
Joseph Steinmeyer <jodalyst@mit.edu>
2017-07-04 21:36:19 -0400

    basic functionality of ss updating added

--- lti.js
+++ lti.js
@@ -1,17 +1,22 @@
 
 
-var x_next = math.matrix([[0],[0],[0]]);
-var x = math.matrix([[0],[0],[0]]);
-var A = math.matrix([[1, 0.01, 0],[0,0.5,0.1],[0,0,0.1]]);
-var B = math.matrix([[0],[0],[1]]);
-var C = math.matrix([1,0,0]);
+var x_next = [0,0,0];
+var x = [0,0,0];
+var A = [[1, 0.01, 0],[0,0.5,0.1],[0,0,0.1]];
+var B = [0,0,1];
+var C = [1,0,0];
 var u = 1;
-var y = 0;
+var y = [0];
 console.log("start");
 for(var i=0;i<100;i++){ 
-    x_next = math.multiply(A,x) + math.multiply(B,u);
-    y = math.multiply(C,x);
+    console.log('loop');
+    var first = numeric.dot(A,x);
+    var second = numeric.dot(B,u);
+    console.log(first);
+    console.log(second);
+    x_next = numeric.add(first,second);
+    y = numeric.dot(C,x);
     x = x_next;
-    console.log(math.index(y);
+    console.log(y);
 }
 
